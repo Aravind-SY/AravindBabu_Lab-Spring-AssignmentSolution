0b0cdbb710d4f28dc2542320a1615cac3941cb41
Aravind-SY <93550319+Aravind-SY@users.noreply.github.com>
2022-04-02 05:14:16 +0530

    Add files via upload

---
+++ college-event/src/test/java/com/greatlearning/EventManagement/controller/CollegeEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.EventManagement.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
